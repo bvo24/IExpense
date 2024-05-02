0d044355e0810adc53a285bb7e48fdb682823796
bvo24 <brianvo2020@gmail.com>
2024-05-02 15:46:35 -0400

    Initial Commit

---
+++ IExpense/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IExpense
+//
+//  Created by Brian Vo on 5/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
