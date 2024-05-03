f6d6fdd32b08c2750fc6cc72c3fc3602da7b10c4
bvo24 <brianvo2020@gmail.com>
2024-05-02 20:51:58 -0400

    Day 38 -Challenge 3 is a WIP

---
+++ IExpense/AddView.swift
@@ -0,0 +1,63 @@
+//
+//  AddView.swift
+//  IExpense
+//
+//  Created by Brian Vo on 5/2/24.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    var expenses : Expenses
+    
+    let types = ["Personal", "Business"]
+    
+    var body: some View {
+            
+        NavigationStack{
+            Form{
+                
+                TextField("Name: ", text: $name)
+                Picker("Type: ", selection: $type){
+                    ForEach(types, id: \.self){
+                        Text($0)
+                    }
+                }
+                TextField("Amount: ", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.numberPad)
+                
+                
+                
+            }
+            .navigationTitle("Add new expense")
+            .toolbar{
+                Button("Save"){
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    
+                    expenses.items.append(item)
+                    dismiss()
+                }
+            }
+            
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+    }
+}
+
+#Preview {
+    AddView(expenses: Expenses())
+}
--- IExpense/ContentView.swift
+++ IExpense/ContentView.swift
@@ -7,15 +7,90 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct ExpenseItem: Identifiable, Codable{
+    var id = UUID()
+    let name : String
+    let type : String
+    let amount : Double
+}
+
+@Observable
+class Expenses{
+    var items = [ExpenseItem](){
+        didSet{
+            if let encoded = try? JSONEncoder().encode(items){
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
         }
-        .padding()
+        
+    }
+    
+    init(){
+        if let savedItems = UserDefaults.standard.data(forKey: "Items"){
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems){
+                items = decodedItems
+                return
+            }
+            
+        }
+        items = []
+    }
+    
+    
+    
+    
+}
+
+
+
+
+
+
+struct ContentView: View {
+    @State private var expenses = Expenses()
+    
+    @State private var showingAddExpense = false
+    
+    
+    var body: some View {
+        NavigationStack{
+            
+            List{
+                
+                ForEach(expenses.items){item in
+                    HStack{
+                        VStack(alignment: .leading){
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                            
+                            
+                        }
+                        Spacer()
+                        
+                        Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                            .foregroundStyle(item.amount >= 100 ? . red : item.amount < 100 && item.amount > 10 ? .yellow : .green   )
+                    }
+                    
+                }
+                .onDelete(perform: removeItem)
+                
+            }
+            .navigationTitle("IExpense")
+            .toolbar{
+                Button("Add Expense", systemImage: "plus"){
+                    showingAddExpense = true
+                }
+            }
+            .sheet(isPresented: $showingAddExpense){
+                AddView(expenses: expenses)
+                
+            }
+        }
+    }
+    
+    func removeItem(at offset : IndexSet){
+        expenses.items.remove(atOffsets: offset)
     }
 }
 
